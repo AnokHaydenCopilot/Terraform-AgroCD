3e2f688755d46f126a36e67f6851f0b18c80d4c4
Modervital <vitalii.yankovskyi.pp.2022@edu.lpnu.ua>
2025-06-13 01:18:30 -0800

    екзамен

--- gcp-terraform-project/environments/dev/backend.tf
+++ gcp-terraform-project/environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket  = "gke-terraform-state-bucket-883" 
+    bucket  = "ekzamen-gcp-terraform" 
     prefix  = "kubernetes-cluster-pipeline/state"   
   }
 }
--- gcp-terraform-project/environments/dev/outputs.tf
+++ gcp-terraform-project/environments/dev/outputs.tf
@@ -28,21 +28,3 @@ output "how_to_get_kubeconfig" {
   description = "Команда для отримання kubeconfig файлу для доступу до кластера."
   value       = "gcloud container clusters get-credentials ${google_container_cluster.primary.name} --region ${google_container_cluster.primary.location} --project ${var.project_id}"
 }
-
-data "kubernetes_service_v1" "grafana_service" {
-  provider = kubernetes 
-  metadata {
-    name      = "prometheus-stack-grafana" 
-    namespace = "monitoring"               
-  }
-  depends_on = [helm_release.prometheus_stack] 
-}
-
-output "grafana_access_info" {
-  description = "Інформація для доступу до Grafana."
-  value = <<EOT
-Grafana доступна за адресою: http://${try(data.kubernetes_service_v1.grafana_service.status[0].load_balancer[0].ingress[0].ip, "PENDING_IP")}
-Ви можете перевірити статус сервісу командою: kubectl get svc prometheus-stack-grafana -n monitoring
-EOT
-  depends_on = [data.kubernetes_service_v1.grafana_service]
-}
