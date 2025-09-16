3bd89b6f858358e14cf8994f6891848110d92a0a
Modervital <vitalii.yankovskyi.pp.2022@edu.lpnu.ua>
2025-09-16 08:26:59 -0800

    Integrate ArgoCD provider and update app port to 8080
    Added the ArgoCD Terraform provider and resources to automate application deployment via ArgoCD. Updated Kubernetes deployment and service to use port 8080 instead of 80. Removed obsolete cloudbuild.yaml CI configuration.

--- on-premise-terraform-project/environments/dev/main.tf
+++ on-premise-terraform-project/environments/dev/main.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "hashicorp/helm"
       version = ">= 2.9.0"
     }
+    argocd = {
+      source  = "oboukili/argocd"
+      version = ">= 5.0.0"
+    }
   }
 }
 provider "kubernetes" {
@@ -33,8 +37,70 @@ resource "helm_release" "argocd" {
   chart      = "argo-cd"
   namespace  = kubernetes_namespace.argocd.metadata[0].name
   version    = "5.51.2"
+  
+  values = [
+    yamlencode({
+      server = {
+        service = {
+          type = "LoadBalancer"
+        }
+      }
+    })
+  ]
 
   depends_on = [
     kubernetes_namespace.argocd,
   ]
+}
+data "kubernetes_service" "argocd_server" {
+  metadata {
+    name      = "argocd-server"
+    namespace = "argocd"
+  }
+  depends_on = [helm_release.argocd]
+}
+
+data "kubernetes_secret" "argocd_initial_admin_secret" {
+  metadata {
+    name      = "argocd-initial-admin-secret"
+    namespace = "argocd"
+  }
+  depends_on = [helm_release.argocd]
+}
+
+provider "argocd" {
+  server_addr = "${data.kubernetes_service.argocd_server.status[0].load_balancer[0].ingress[0].hostname}:${data.kubernetes_service.argocd_server.spec[0].port[0].port}"
+  username    = "admin"
+  password    = data.kubernetes_secret.argocd_initial_admin_secret.data.password
+  insecure    = true
+}
+
+resource "argocd_application" "my_simple_app" {
+  metadata {
+    name      = "my-simple-app"
+    namespace = "argocd"
+  }
+
+  spec {
+    project = "default"
+
+    source {
+      repo_url        = "https://github.com/AnokHaydenCopilot/Terraform-AgroCD"
+      path            = "on-premise-terraform-project/source_code_for_pipeline/kubernetes"
+      target_revision = "HEAD"
+    }
+
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = "default"
+    }
+
+    sync_policy {
+      automated {
+        prune = true
+      }
+    }
+  }
+
+  depends_on = [helm_release.argocd]
 }
